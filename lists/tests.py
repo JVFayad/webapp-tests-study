ea558e71db5bbe0ef01ac4a44e04985be9bbf73d
JVFayad <joao.victor.fa2014@gmail.com>
2020-06-10 18:49:05 -0300

    Home using template

--- lists/tests.py
+++ lists/tests.py
@@ -2,20 +2,13 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
+        response = self.client.get('/')
 
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
