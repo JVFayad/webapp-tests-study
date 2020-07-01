b9af29f321fab4b18accd06c625eac8f2bd56265
JVFayad <joao.victor.fa2014@gmail.com>
2020-07-01 19:29:49 -0300

    Add list model with item association

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -10,10 +10,12 @@ def home_page(request):
 
 def view_list(request):
     items = Item.objects.all()
+    
     return render(request, 'list.html', {'items': items})
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list = list_)
 
     return redirect('/lists/the-only-list-in-the-world/')
