9a558add7f5a75cfea85648f9abc47bf2645c1ec
JVFayad <joao.victor.fa2014@gmail.com>
2020-07-30 20:12:31 -0300

    Add Django Heroku

--- superlists/settings.py
+++ superlists/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
